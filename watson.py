401055ac12a7696fd9ab38d552c0e377a5262812
Joshua Koh <Joshua@kohfamily.com>
2016-09-24 09:58:38 -0400

    "We are pulling from Watson, over."

--- watson.py
+++ watson.py
@@ -3,8 +3,8 @@ from watson_developer_cloud import ToneAnalyzerV3
 
 
 tone_analyzer = ToneAnalyzerV3(
-   username='YOUR SERVICE USERNAME',
-   password='YOUR SERVICE PASSWORD',
-   version='2016-05-19 ')
+   username='f74339b8-848d-4622-9e43-2fe02e958b0c',
+   password='pKZ4jVwiJzV7',
+   version='2016-05-19')
 
 print(json.dumps(tone_analyzer.tone(text='A word is dead when it is said, some say. Emily Dickinson'), indent=2))
