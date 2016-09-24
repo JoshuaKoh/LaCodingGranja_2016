5c2e8c2cc3bbd71a408d96786cf101e6b7e44000
Andrew Oliveira <tf2zone@gmail.com>
2016-09-24 14:49:20 -0400

    Created addFoxStorys()
    Created the funciton to maintain previous functionallity so I could add a new type of site

--- Scrapper.py
+++ Scrapper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from dbco import *
 from datetime import datetime
 
+
 class Story:
     'Common base class for all Stories'
     storyCount = 0
@@ -22,7 +23,7 @@ class Story:
         print("Total Stories %d" % Story.storyCount)
 
 
-def makeStory(url):
+def makeStoryFoxMobile(url):
     "turns a url into a story object"
 
     html = requests.get(url).text
@@ -48,50 +49,66 @@ def makeStory(url):
     return out
 
 
-def getNextStory(story):
-    "creates a story object of the next story"
-    out = makeStory(story.nextUrl)
+def makeStoryCNN(url):
+    "turns a url into a story object"
+
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, "lxml")
+    elements = soup.findAll("div", {"class": "element"})
+    # print("\n" + element.get_text() + "\n")
+    title = elements[0].get_text().strip()
+    author = elements[1].get_text().strip()
+    date = elements[2].get_text().strip()
+    body = elements[3].get_text().strip()
+    out = Story(url, title, date, author, body, "")
 
     return out
 
 
-bulk = articles.initialize_unordered_bulk_op()
+def getNextStory(story):
+    "creates a story object of the next story"
+    out = makeStoryFoxMobile(story.nextUrl)
 
-# input("Please enter a URL")
-site = "http://www.foxnews.mobi/quickPage.html?page=38321&content=118383004&pageNum=-1"
-# print(site)
-myStory = makeStory(site)
+    return out
 
-storyDoc = {
-        "url": myStory.url
-        "title": myStory.title,
-        "author": myStory.author
-        "date": myStory.date
-        "body": myStory.body
-        "links": myStory.links
-        "nextUrl": myStory.nextUrl
-        "dateFetched": myStory.dateFetched}
-bulk.insert(storyDoc)
 
-contStory = myStory
+def addFoxStorys():
+    bulk = articles.initialize_unordered_bulk_op()
 
-for i in range(1000000):
+    # input("Please enter a URL")
+    site = "http://www.foxnews.mobi/quickPage.html?page=38321&content=118383004&pageNum=-1"
+    # print(site)
+    myStory = makeStoryFoxMobile(site)
 
-    contStory = getNextStory(contStory)
     storyDoc = {
-        "url": contStory.url
-        "title": contStory.title,
-        "author": contStory.author
-        "date": contStory.date
-        "body": contStory.body
-        "links": contStory.links
-        "nextUrl": contStory.nextUrl
-        "dateFetched": contStory.dateFetched}
+        "url": myStory.url,
+        "title": myStory.title,
+        "author": myStory.author,
+        "date": myStory.date,
+        "body": myStory.body,
+        "links": myStory.links,
+        "nextUrl": myStory.nextUrl,
+        "dateFetched": myStory.dateFetched}
     bulk.insert(storyDoc)
-    print("STORY NUMBER: " + str((i + 2)))
-    # input()
-try:
-    bulk.execute()
-except BulkWriteError as bwe:
-    pprint(bwe.details) 
 
+    contStory = myStory
+
+    for i in range(1000000):
+
+        contStory = getNextStory(contStory)
+        storyDoc = {
+            "url": contStory.url,
+            "title": contStory.title,
+            "author": contStory.author,
+            "date": contStory.date,
+            "body": contStory.body,
+            "links": contStory.links,
+            "nextUrl": contStory.nextUrl,
+            "dateFetched": contStory.dateFetched}
+        bulk.insert(storyDoc)
+        print("STORY NUMBER: " + str((i + 2)))
+        # input()
+    try:
+        bulk.execute()
+    except BulkWriteError as bwe:
+        pprint(bwe.details)
