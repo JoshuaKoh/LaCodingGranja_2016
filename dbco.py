d29bc13424410d9ee5333ce5aea9925d80d910f0
Joshua Koh <Joshua@kohfamily.com>
2016-09-24 16:18:18 -0400

    Updated datavis

--- dbco.py
+++ dbco.py
@@ -1,4 +1,4 @@
 from pymongo import MongoClient
-client = MongoClient("")
+client = MongoClient("mongodb://newsmood:hLQp9PBtdRnJGmKI9FegaJJPLI9T3Yl8vGHrwcXzYLt41dUXYZTzsoA27NrH001CxZbgA0Aqjbio4liIVxKkIA==@newsmood.documents.azure.com:10250/?ssl=true")
 db = client['admin']
 articles = db.articles
