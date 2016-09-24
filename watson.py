a209343a0c0d15175dd88645661b649ee4a5675c
David <davidisrawi@gmail.com>
2016-09-24 12:36:58 -0400

    article -> analyzed article with Watson

--- watson.py
+++ watson.py
@@ -1,10 +1,56 @@
 import json
 from watson_developer_cloud import ToneAnalyzerV3
 
+news = open('andrewsJSON.json', 'r')
 
 tone_analyzer = ToneAnalyzerV3(
    username='f74339b8-848d-4622-9e43-2fe02e958b0c',
    password='pKZ4jVwiJzV7',
    version='2016-05-19')
 
-print(json.dumps(tone_analyzer.tone(text='A word is dead when it is said, some say. Emily Dickinson'), indent=2))
+class Article(object):
+    url = "",
+    title = "",
+    date = "",
+    emotional_sentence = "",
+    anger = 0,
+    disgust = 0,
+    fear = 0,
+    joy = 0,
+    sadness = 0,
+    openness = 0,
+    emotional_range = 0
+
+def classifyArticle(news):
+    article = Article()
+    article.url = news.url
+    article.title = news.title
+    article.date = news.dateCreated
+
+    analyzed = tone_analyzer.tone(news.text)
+    article.anger = analyzed.document_toned.tone_categories[0].tones[0].score
+    article.disgust = analyzed.document_toned.tone_categories[0].tones[1].score
+    article.fear = analyzed.document_toned.tone_categories[0].tones[2].score
+    article.joy = analyzed.document_toned.tone_categories[0].tones[3].score
+    article.sadness = analyzed.document_toned.tone_categories[0].tones[4].score
+    article.emotional_range = analyzed.document_toned.tone_categories[2].tones[4].score
+
+    onEdge = 0  # Find most emotional sentence - stores highest emotional_range value
+    # Checks the emotional range
+    for sentence in analyzed.sentences_tone:
+        sentenceEmotional = sentence.tone_categories[2].tones[4].score
+        if sentenceEmotional > onEdge:
+            onEdge = sentenceEmotional
+            article.emotional_sentence = sentence.text
+    return article
+
+analyzed_articles = list()
+
+for newsArticle in news:
+    analyzed_articles.append(classifyArticle(newsArticle))
+
+with open('analyzed_articles.json', 'w') as outfile:
+    json.dump(analyzed_articles, outfile, default=lambda o: o.__dict__, indent=2)
+
+# For example output, print this
+# print(json.dumps(tone_analyzer.tone(text='I am really excited because David is here and he is my best friend and I love him so much! Yay!'), indent=2))
