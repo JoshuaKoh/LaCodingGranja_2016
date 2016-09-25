bbef8768bf289a60a169e660c89b0ece231c546d
David <davidisrawi@gmail.com>
2016-09-25 08:35:39 -0400

    Single page works

--- website/FlavoredNews/imports/ui/article-detail.js
+++ website/FlavoredNews/imports/ui/article-detail.js
@@ -9,6 +9,29 @@ import './article-detail.html';
 
 Template.articleDetail.onCreated(function(){
 
+    google.charts.load("current", {packages:["corechart"]});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ["Emotion", "Percentage"],
+        ["Anger", this.anger],
+        ["Joy", this.joy],
+        ["Fear", this.fear],
+        ["Disgust", this.disgust],
+        ["Sadness", this.sadness]
+      ]);
+
+      var view = new google.visualization.DataView(data);
+
+      var options = {
+        title: "Emotion of the news",
+        width: 600,
+        height: 400,
+        bar: {groupWidth: "95%"}
+      };
+      var chart = new google.visualization.BarChart(document.getElementById("MoodGraph"));
+      chart.draw(view, options);
+  }
 });
 
 Template.articleDetail.helpers({
--- website/FlavoredNews/server/main.js
+++ website/FlavoredNews/server/main.js
@@ -4,5 +4,4 @@ import { Mongo } from 'meteor/mongo';
 Meteor.startup(() => {
   // code to run on server at startup
   Articles = new Mongo.Collection("articles");  //Connect to the articles collection
-  console.log(Articles.findOne().title);
 });
