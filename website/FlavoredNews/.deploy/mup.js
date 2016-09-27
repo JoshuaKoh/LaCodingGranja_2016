a794f525d3ac1d5c6d3fce2cc57160d58c7c30f4
David <davidisrawi@gmail.com>
2016-09-26 23:02:04 -0400

    deployed

--- website/FlavoredNews/.deploy/mup.js
+++ website/FlavoredNews/.deploy/mup.js
@@ -2,16 +2,16 @@ module.exports = {
   servers: {
     one: {
       host: '13.84.47.59',
-      username: 'flavorednews',
+      username: 'flavoredadmin',
       // pem:
-      password:'Flavorednews16',
+      password:'Flavoredadmin16'
       // or leave blank for authenticate from ssh-agent
     }
   },
 
   meteor: {
-    name: 'Flavored News',
-    path: '../../FlavoredNews',
+    name: 'FlavoredNews',
+    path: '../',
     servers: {
       one: {}
     },
@@ -19,12 +19,12 @@ module.exports = {
       serverOnly: false,
     },
     env: {
-      ROOT_URL: 'flavorednews.com',
+      ROOT_URL: 'http://13.84.47.59',
       MONGO_URL: 'mongodb://newsmood:hLQp9PBtdRnJGmKI9FegaJJPLI9T3Yl8vGHrwcXzYLt41dUXYZTzsoA27NrH001CxZbgA0Aqjbio4liIVxKkIA==@newsmood.documents.azure.com:10250/?ssl=true'
     },
     docker: {
       image:'abernix/meteord:base'
-    }
+    },
 
     //dockerImage: 'kadirahq/meteord'
     deployCheckWaitTime: 60
@@ -34,7 +34,7 @@ module.exports = {
     oplog: true,
     port: 27017,
     servers: {
-      one: {},
+      one: {}
     },
   },
 };
